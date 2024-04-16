aeefcba9b5e75184005535ddf26c5176d26c94f0
Tran Lam <lam.tran.113520@gmail.com>
2024-04-17 01:58:25 +0700

    update

--- t2.js
+++ t2.js
@@ -34,14 +34,45 @@ function fadeIn(){
         audio.play();
         // Resolve the Promise after the fadeIn animation is finished
         setTimeout(() => {
+            $('.bg_heart').removeClass('hidden')
             $('.mask').css('display', 'none');
+            document.querySelector(".content").onclick = null;
+            document.querySelector("#heart").hidden=true
+            
+            // Fade in the date and time display
+            $('.content').hide().html(displayDateTime()).fadeIn(1000);
+
+            // Animate the padding-top to 0
+            $('.content').attr('style', 'padding-top: 0 !important; padding-bottom: 0 !important;');
+
+            // Animate the background color to #ff4081
+            $('body').css('background-color', '#ff4081');
             resolve();
         }, totalDuration + 480);
     });
    
 };
 
+setInterval(()=>{
+    if(!$('.bg_heart').hasClass('hidden')){
+        displayDateTime();
+        $('.content').html(displayDateTime());
+    }
+   
+}, 1000)
 
+function displayDateTime() {
+    var now = new Date();
+    var date = now.toLocaleDateString('vn-VN');
+    var time = now.toLocaleTimeString('vn-VN');
+    var day = now.toLocaleDateString('vn-VN', { weekday: 'long' });
+    return `
+        <div> ${day} </div>
+        <div>${date} - ${time}</div>
+       
+        
+    `;
+}
 
 document.querySelector(".content").onclick=  ()=>{
     document.querySelector("#heart").hidden=false
